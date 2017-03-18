3897a38c49255268cc7932da68421faa9d610c2c
DorothyChaos <dorothy_terrill@yahoo.com>
2017-03-18 15:39:08 -0400

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -10,5 +10,10 @@ int main(const int argc, const char * argv []);
 * This program will create a text user interface
 */
 int main(const int argc, const char * argv []){
-
+//Will need to be adapted to writing capabilities
+  //Scrolling Functionality Variables
+  string file, line1, line2;
+  int count = 0, maxY = 0, maxX = 0, top = 0, bottom = 0;
+  bool done = FALSE;
+  
 }
