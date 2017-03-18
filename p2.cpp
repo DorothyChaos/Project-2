8c7c60ea6e52b5eeb1f230b3af343a35f64fff9e
DorothyChaos <dorothy_terrill@yahoo.com>
2017-03-18 15:15:14 -0400

    Create p2.cpp

---
+++ p2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ncurses.h>
+/**
+* Prototype of main
+*/
+int main(const int argc, const char * argv []);
+/**
+* This program will create a text user interface
+*/
+int main(const int argc, const char * argv []){
+
+}
